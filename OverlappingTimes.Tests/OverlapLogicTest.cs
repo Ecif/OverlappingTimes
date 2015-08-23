9a3b3dba5f098c9d0ef651b9d1cb1663a6b26c91
Ecif <reva.ardi@gmail.com>
2015-08-23 19:55:04 +0300

    tests

--- OverlappingTimes.Tests/OverlapLogicTest.cs
+++ OverlappingTimes.Tests/OverlapLogicTest.cs
@@ -11,15 +11,19 @@ namespace OverlappingTimes.Tests
     [TestFixture]
     public class OverlapLogicTest
     {
-        public IList<TimesModel> TestList1;
+        private IList<TimesModel> _testList;
+        private DateTime _timeSpanEntryTime;
+        private DateTime _timeSpanLeaveTime;
+        private int _maxNumberOfVisitors = 0;
 
         [SetUp]
-        void Setup()
+        public void Setup()
         {
+            //Arrange
             var year = DateTime.Now.Year;
             var month = DateTime.Now.Month;
             var day = DateTime.Now.Day;
-            TestList1 = new List<TimesModel>
+            _testList = new List<TimesModel>
             {
                 new TimesModel {EnterTime = new DateTime(year, month, day, 11,00,00), LeaveTime = new DateTime(year, month, day,16,00,00)},
                 new TimesModel {EnterTime = new DateTime(year, month, day,13,00,00), LeaveTime = new DateTime(year, month, day,15,00,00)},
@@ -30,21 +34,31 @@ namespace OverlappingTimes.Tests
                 new TimesModel {EnterTime = new DateTime(year, month, day,10,00,00), LeaveTime = new DateTime(year, month, day,12,00,00)},
                 new TimesModel {EnterTime = new DateTime(year, month, day,06,00,00), LeaveTime = new DateTime(year, month, day,14,00,00)}
             };
+
+            _timeSpanEntryTime = new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day, 11, 00, 00);
+            _timeSpanLeaveTime = new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day, 12, 00, 00);
         }
         
         
         [Test]
         public void TestFindMaxOverlaps()
         {
-            var returnedList = OverlapLogic.FindMaxOverlaps(TestList1).ToList();
-            Assert.IsNotNull(returnedList);
-            Assert.IsNotNull(returnedList.FirstOrDefault());
-            Assert.That(returnedList.Count, Is.EqualTo(4));
-            Assert.That(returnedList.FirstOrDefault().Overlaps, Is.EqualTo(3));
-
+            //Act
+            var returnedList = OverlapLogic.FindMaxOverlaps(_testList).ToList();
+            var firstTimeSpan = returnedList.FirstOrDefault();
+            if (firstTimeSpan != null)
+                _maxNumberOfVisitors = firstTimeSpan.Overlaps;
             var thirdTimeSpan = returnedList[2];
-            Assert.That(thirdTimeSpan.EnterTime, Is.EqualTo(new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day, 11, 00,00)));
-            Assert.That(thirdTimeSpan.LeaveTime, Is.EqualTo(new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day, 12,00,00)));
+
+
+            //Assert
+            Assert.IsNotNull(returnedList);
+            Assert.IsNotNull(firstTimeSpan);
+            Assert.That(returnedList.Count, Is.EqualTo(4));
+            Assert.That(_maxNumberOfVisitors, Is.EqualTo(3));
+
+            Assert.That(thirdTimeSpan.EnterTime, Is.EqualTo(_timeSpanEntryTime));
+            Assert.That(thirdTimeSpan.LeaveTime, Is.EqualTo(_timeSpanLeaveTime));
         }
 
     }
