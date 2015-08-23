40f6fa2bb25f78a22b0fc4b6ef1619a1c1fddb9c
Ecif <reva.ardi@gmail.com>
2015-08-23 19:57:46 +0300

    testsm

--- OverlappingTimes.Tests/TimesModelTest.cs
+++ OverlappingTimes.Tests/TimesModelTest.cs
@@ -14,6 +14,7 @@ namespace OverlappingTimes.Tests
         [Test]
         public void TestIntersect()
         {
+            // Arrange
             var year = DateTime.Now.Year;
             var month = DateTime.Now.Month;
             var day = DateTime.Now.Day;
@@ -22,12 +23,15 @@ namespace OverlappingTimes.Tests
             var timesModel3 = new TimesModel {EnterTime = new DateTime(year, month, day, 10, 30, 00), LeaveTime = new DateTime(year, month, day, 10, 50, 00)};
             var timesModel4 = new TimesModel {EnterTime = new DateTime(year, month, day, 09, 30, 00), LeaveTime = new DateTime(year, month, day, 15, 00, 00)};
 
-            var isOverlapping = timesModel1.Intersects(timesModel2);
-            Assert.IsTrue(isOverlapping);
-            isOverlapping = timesModel1.Intersects(timesModel3);
-            Assert.IsFalse(isOverlapping);
-            isOverlapping = timesModel4.Intersects(timesModel2);
-            Assert.IsTrue(isOverlapping);
+            // Act
+            var isOverlapping1 = timesModel1.Intersects(timesModel2);
+            var isOverlapping2 = timesModel1.Intersects(timesModel3);
+            var isOverlapping3 = timesModel4.Intersects(timesModel2);
+
+            // Assert
+            Assert.IsTrue(isOverlapping1);
+            Assert.IsFalse(isOverlapping2);
+            Assert.IsTrue(isOverlapping3);
 
         }
     }
